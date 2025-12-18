f1455fd2f1ebfc3216f379a406e461e47ad2846b
MillSher <gureevm56@gmail.com>
2025-12-18 07:13:24 +0300

    полное обновление

--- app/handlers.py
+++ app/handlers.py
@@ -1,19 +1,32 @@
+import sys
+
+from aiogram import Bot
 from aiogram import F, Router
 from aiogram.types import Message, ReplyKeyboardRemove
 from aiogram.filters import CommandStart, Command
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
+
+
 from app.generate import ai_generate
 from keyboards.command_keyboards import ButtonText, get_on_start_kb
 
+
+from config import TG_TOKEN
+
+
+
+
+
 router = Router()
 
+bot = Bot(token=TG_TOKEN)
 
 class Gen(StatesGroup):
     wait = State()
 
 
-
+#Команда "старт" начало бота
 
 @router.message(CommandStart())
 async def cmd_start(message: Message):
@@ -22,6 +35,9 @@ async def cmd_start(message: Message):
         reply_markup=get_on_start_kb(),
     )
 
+
+#Узнать что делает бот
+
 @router.message(F.text == ButtonText.WATS_NEXT)
 @router.message(Command('help'))
 async def cmd_help(message: Message):
@@ -29,26 +45,31 @@ async def cmd_help(message: Message):
 
 
 
+#Команда "стоп" - остановить бота
 
 @router.message(F.text == ButtonText.STOP)
 @router.message(Command('stop'))
-async def cmd_stop(message: Message):
+async def cmd_stop(message: Message, state: FSMContext):
+    await state.clear()
     await message.answer(
         text='Если нужна помощь нажмите: /start',
-        reply_markup=ReplyKeyboardRemove(),
+        reply_markup=ReplyKeyboardRemove()
     )
+    sys.exit(0)
 
 
+#Задать вопрос нейросети
 
-@router.message(Gen.wait)
-async def stop_flood(message: Message):
-    await message.answer(text='Подождите, запрос обрабатываестя.')
-
-
-
-@router.message()
-async def generating(message: Message, state: FSMContext):
+@router.message(F.text == ButtonText.QUESTION)
+@router.message(Command('ask'))
+async def ask_start(message: Message, state: FSMContext):
     await state.set_state(Gen.wait)
+    await message.answer('Задайте ваш вопрос нейросети.')
+
+
+#Анти-спам
+@router.message(Gen.wait)
+async def generating(message: Message, state: FSMContext):
     response = await ai_generate(message.text)
     await message.answer(response)
     await state.clear()
--- keyboards/command_keyboards.py
+++ keyboards/command_keyboards.py
@@ -3,16 +3,20 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 class ButtonText:
     HELLO = 'Hello!'
-    WATS_NEXT = "What's next?"
+    WATS_NEXT = "About"
     STOP = 'Stop'
+    QUESTION = 'Question'
 
 
+
+#Кнопки вместо клавиатуры в тг
 def get_on_start_kb():
     button_hello = KeyboardButton(text=ButtonText.HELLO)
     button_help = KeyboardButton(text=ButtonText.WATS_NEXT)
     button_stop = KeyboardButton(text=ButtonText.STOP)
+    button_question = KeyboardButton(text=ButtonText.QUESTION)
     buttons_first_row = [button_hello, button_help]
-    buttons_second_row = [button_stop]
+    buttons_second_row = [button_stop, button_question]
     markup = ReplyKeyboardMarkup(
         keyboard=[buttons_first_row, buttons_second_row],
         resize_keyboard=True,
